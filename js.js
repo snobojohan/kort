f0b512804365082ed5474ee718521bda2fc6d1bb
snobojohan <johan.hallberg@gmail.com>
2014-11-03 14:29:33 +0100

    Testing

--- js.js
+++ js.js
@@ -2,18 +2,22 @@ $(function() {
 
     console.log( "ready to go!" );
 
+
     $(document).autoBars(function() {
         var $html = $.handlebarTemplates['main']({
             message: "This is my message"
         });
         $('body').append($html);
+
+		// FOR TEST
+		$("#Global_Content").addClass("showme");
+		$("#Global_Blocker").fadeIn(100);
     });
 
     $( "body" ).on( "click", "#Global_Clicker", function(e) {
 	  	// console.log( "Handler for .click() called." );
 	  	$("#Global_Content").addClass("showme");
 	  	$("#Global_Blocker").fadeIn(100);
-	  	// $(".play_root").addClass("opac");
 	  	return false;
 	});
 	
